187057f03bd1f741e3ba5fa91d7d1d3ca1799838
Sergey <ben7990@rambler.ru>
2023-04-27 00:03:59 +0300

    DZ_4_Turbin

---
+++ work.sql
@@ -0,0 +1,14 @@
+-- create
+CREATE TABLE EMPLOYEE (
+id INTEGER PRIMARY KEY,
+name TEXT NOT NULL,
+age TEXT NOT NULL,
+address TEXT NOT NULL
+);
+-- insert
+INSERT INTO EMPLOYEE VALUES (0001, 'Александр', '22', 'Пролетарск');
+INSERT INTO EMPLOYEE VALUES (0002, 'Борис', '48', 'Сальск');
+INSERT INTO EMPLOYEE VALUES (0003, 'Виолетта', '19', 'Миллерово');
+INSERT INTO EMPLOYEE VALUES (0004, 'Галина', '31', 'Зверево');
+INSERT INTO EMPLOYEE VALUES (0005, 'Дмитрий', '50', 'Волгодонск');
+INSERT INTO EMPLOYEE VALUES (0006, 'Евлампия', '66', 'Старочеркасск');
